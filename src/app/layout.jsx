a7fcb9367065c11daa8cf2b0016e5d9b6584c921
zzrose <zz134124@outlook.com>
2023-06-20 12:47:45 -0400

    add dark mode

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,6 +2,7 @@
 import Header from '@/components/Header'
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Providers from './Providers'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,15 +15,17 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
 
-      <body className={inter.className}>{children}
+      <body className={inter.className}>
+        <Providers>
+          {/* Header */}
+          <Header />
 
-        {/* Header */}
-        <Header></Header>
+          {/* Navbar */}
 
-        {/* Navbar */}
+          {/* SearchBox */}
 
-        {/* SearchBox */}
-      
+          { children }
+        </Providers>
       </body>
     </html>
   )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,7 @@ import MenuItem from './MenuItem'
 import { AiFillHome } from 'react-icons/ai'
 import { BsFillInfoCircleFill } from 'react-icons/bs'
 import Link from 'next/link'
+import DarkModelSwitch from './DarkModelSwitch'
 
 export default function Header() {
   return (
@@ -11,7 +12,8 @@ export default function Header() {
         <MenuItem title="HOME" address="/" Icon={AiFillHome}/>
         <MenuItem title="ABOUT" address="/about" Icon={BsFillInfoCircleFill}/>
       </div>
-      <div className=''>
+      <div className='flex items-center space-x-5'>
+        <DarkModelSwitch />
         <Link href="/">
           <h2>
             <span className='font-bold bg-amber-500 py-1 px-2 rounded-lg mr-1'>iMDB</span>
